20839cd4a8cbc205dedb69ae76c8fe590621cfc4
Tom Hole <git@stirtingale.com>
2019-09-10 20:17:56 +0200

    Line by line output. Otherwise it just outputs the first line

--- index.php
+++ index.php
@@ -23,34 +23,37 @@
 
 
 	<p><strike>Lazy</strike> Simple script to convert input.csv (from Capital on Tap) to a FreeAgent friendly CSV for import.</p>
-
+	<?php 
+	$outputfile_location = fopen("output.csv", "w") or die("Unable to open file!");
+	?>
 	<code>
 	<?php 
 	$row = 1;
 	$counter = 0;
-	$output = null;
+	$complete_output = null;
 	if (($handle = fopen("input.csv", "r")) !== FALSE) {
 	  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 	  	$counter++;
+	  	$output = null; // reset
 	  	if($counter!=1){
 	  		$output .= $data[0].","; // date
 	  		$amount = abs($data[2]) * -1; // invert
 	  		$output .= $amount.","; // amount
 	  		$output .= $data[1]; // description
 	  		$output .= "<br>";
+	  		$complete_output .= $output;
+			fwrite($outputfile_location, $output);
 		}
 	  }
 	  fclose($handle);
 	}
 
-	echo $output;
+	echo $complete_output;
 	?>
 	</code>
 	
 	<p>
-		<?php 
-		$outputfile_location = fopen("output.csv", "w") or die("Unable to open file!");
-		fwrite($outputfile_location, $output);
+		<?php
 		fclose($outputfile_location);
 		$cachebuster = time();
 		 ?>		
